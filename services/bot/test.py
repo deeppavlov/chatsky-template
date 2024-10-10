007c5ea19d67f1173dccf0bef26fb05608e0fe1a
Roman Zlobin <RLKRo@proton.me>
2024-10-10 14:56:52 +0300

    delete env vars during tests

--- services/bot/test.py
+++ services/bot/test.py
@@ -4,8 +4,6 @@ from pathlib import Path
 from chatsky.utils.testing import check_happy_path
 import pytest
 
-from .bot.pipeline import pipeline
-
 
 TEST_DATA_FILE = Path(__file__).parent / "test_data.json"
 
@@ -20,5 +18,11 @@ assert len(TEST_CASES) == 3
 @pytest.mark.parametrize(
     "happy_path", TEST_CASES
 )
-def test_happy_paths(happy_path: str):
+def test_happy_paths(happy_path: str, monkeypatch):
+    monkeypatch.delenv("TG_BOT_TOKEN_FILE", raising=False)
+    monkeypatch.delenv("TG_BOT_TOKEN", raising=False)
+    monkeypatch.delenv("DB_URI", raising=False)
+
+    from .bot.pipeline import pipeline
+
     check_happy_path(pipeline, TEST_DATA[happy_path])
